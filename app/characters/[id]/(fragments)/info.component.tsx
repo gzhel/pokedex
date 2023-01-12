550bae9db7a25ab8bdd46ad5e313d65610a7f561
gzhel <gregoryzhel@gmail.com>
2023-01-12 18:52:51 +0700

    feature/PD-13 - character details page cover image

--- app/characters/[id]/(fragments)/info.component.tsx
+++ app/characters/[id]/(fragments)/info.component.tsx
@@ -3,6 +3,7 @@
 import { memo } from "react";
 import s from "./info.module.scss";
 import cn from "classnames";
+import { Typography } from "@typography/typography";
 
 interface CharacterDetailsInfo {
   character: CharacterParsed;
@@ -13,20 +14,73 @@ interface CharacterDetailsInfo {
 const CharacterDetailsInfo = memo<CharacterDetailsInfo>((props) => {
   const { character, locations, habitat } = props;
 
+  // TODO:
+  //  1) If API haven't habitat, application crashes
+  //  2) Collectibles page items should have cm / kg dimensions
+
   return (
     <section className={cn("mx-auto", s.coverBorder)}>
       <div className={cn("px-10 py-10", s.coverLayout)}>
-        <p>Name: {character.name}</p>
-        <p>ID: {character.id}</p>
-        <p>Height: {character.height}</p>
-        <p>Weight: {character.weight}</p>
-        <p>Habitat: {habitat.name}</p>
-        <p>
-          Location:{" "}
-          {locations.locations.map((location) => (
-            <span key={location}>{location.split("-").join(" ")}, </span>
-          ))}
+        <p className={s.coverItem}>
+          <Typography tag={"span"} variant={"textBig"} className={"font-bold"}>
+            Name:
+          </Typography>
+          <Typography tag={"span"} variant={"textBig"} className={"ml-2"}>
+            {character.name[0].toUpperCase() + character.name.slice(1)}
+          </Typography>
         </p>
+        <p className={s.coverItem}>
+          <Typography tag={"span"} variant={"textBig"} className={"font-bold"}>
+            ID:
+          </Typography>
+          <Typography tag={"span"} variant={"textBig"} className={"ml-2"}>
+            {character.id}
+          </Typography>
+        </p>
+        <p className={s.coverItem}>
+          <Typography tag={"span"} variant={"textBig"} className={"font-bold"}>
+            Height:
+          </Typography>
+          <Typography tag={"span"} variant={"textBig"} className={"ml-2"}>
+            {character.height && character.height * 10} cm
+          </Typography>
+        </p>
+        <p className={s.coverItem}>
+          <Typography tag={"span"} variant={"textBig"} className={"font-bold"}>
+            Weight:
+          </Typography>
+          <Typography tag={"span"} variant={"textBig"} className={"ml-2"}>
+            {character.weight && character.weight / 10} kg
+          </Typography>
+        </p>
+        <p className={s.coverItem}>
+          <Typography tag={"span"} variant={"textBig"} className={"font-bold"}>
+            Habitat:
+          </Typography>
+          <Typography tag={"span"} variant={"textBig"} className={"ml-2"}>
+            {habitat.name}
+          </Typography>
+        </p>
+        {!locations.locations.length ? null : (
+          <p className={s.coverItem}>
+            <Typography
+              tag={"span"}
+              variant={"textBig"}
+              className={"font-bold"}
+            >
+              Locations:
+            </Typography>
+            <Typography tag={"span"} variant={"textBig"} className={"ml-2"}>
+              {locations.locations.map((location, idx, arr) => (
+                <span key={location}>
+                  {location[0].toUpperCase() +
+                    location.slice(1).split("-").join(" ")}
+                  {arr[idx + 1] ? ", " : ""}
+                </span>
+              ))}
+            </Typography>
+          </p>
+        )}
       </div>
     </section>
   );
